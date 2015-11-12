bb8f9dd89cef6b2458e54d23e5376201b00cf9c5
Alex Olivas <alex.olivas11@gmail.com>
2015-11-12 17:29:22 -0500

    Added accounts app to base settings + removed commented out code

--- djangorest/apps/authentication/auth_urls.py
+++ djangorest/apps/authentication/auth_urls.py
@@ -1,14 +1,8 @@
 from django.conf.urls import url
-
 from djangorest.apps.authentication import views
 
 
-
-# Login is handled by curl -X POST http://localhost:8000/api-token-auth/ -d "password=admin&username=admin"??
-# This should not be handled by that because I need to return the user object
 urlpatterns = [
     url(r'^login/', views.LoginView.as_view(), name='login'),
     url(r'^logout/', views.LogoutView.as_view(), name='logout'),
-    # url(r'^create/user/', views.CreateAccountView.as_view(), name='create'),
-    # url(r'^delete/user/', views.UpdateAccountView.as_view(), name='update'),
 ]
--- djangorest/settings/base.py
+++ djangorest/settings/base.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'djangorest.apps.authentication',
+    'djangorest.apps.accounts',
 )
 
 MIDDLEWARE_CLASSES = (
