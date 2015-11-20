bd34c1d4ccf23656718158a29d2b3a3e52a887fb
Alex Olivas <alex.olivas11@gmail.com>
2015-11-20 01:11:07 -0500

    Added rest_framework_swagger

--- djangorest/settings/base.py
+++ djangorest/settings/base.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',
     'rest_framework.authtoken',
     'apps.authentication',
     'apps.accounts',
