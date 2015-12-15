ce3d7bad174cd9c0ed03bed68d779b32e22f1bff
Alex Olivas <alex.olivas11@gmail.com>
2015-12-15 00:56:05 -0600

    Additional serialization to the login process: more user info, client data, and user/system preferences

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,18 +1,57 @@
-from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework import serializers
-# from users.serializers import UserSerializer
+from accounts.models import Client, User, ClientMembership
+
+"""
+This serializer file contains metadata information providing a minimal amount of data when
+authenticating into the system, such as names and IDs.
+"""
 
 
-class UserSerializer(serializers.ModelSerializer):
+class ClientMetadataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ('id', 'name')
+        read_only_fields = ('created_date', 'updated_date')
+
+
+class UserMetadataSerializer(serializers.ModelSerializer):
+    is_staff = serializers.SerializerMethodField()
+    is_admin = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'is_staff', 'is_admin')
+
+    def get_is_staff(self, user):
+        if user.is_superuser:
+            # Ignore this logic if the user is an admin
+            return False
+        else:
+            is_staff = False
+            for group in user.groups.all():
+                print str(group.name).lower()
+                if str(group.name).lower() == 'staff':
+                    is_staff = True
+                    break
+            return is_staff
+
+    def get_is_admin(self, user):
+        # If they are superusers then they are admins by default
+        return user.is_superuser
+
+
+class ClientMemberSerializer(serializers.ModelSerializer):
+    client = ClientMetadataSerializer()
+
+    class Meta:
+        model = ClientMembership
+        read_only_fields = ('created_date', 'updated_date')
 
 
 class TokenSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserMetadataSerializer()
 
     class Meta:
         model = Token
-        fields = ('key', 'user')
+        exclude = ('created',)
--- authentication/views.py
+++ authentication/views.py
@@ -6,8 +6,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.throttling import AnonRateThrottle
 from django.contrib.auth import authenticate
-from accounts.models import UserAccountSetup
-from serializers import TokenSerializer
+from django.contrib.auth.models import User
+from serializers import TokenSerializer, ClientMemberSerializer
+from accounts.models import ClientMembership, UserPreferences
+from accounts.serializers import UserPreferencesSerializer
+from system.models import SystemPreferences
+from system.serializers import SystemPreferencesSerializer
 
 
 class LoginView(APIView):
@@ -21,29 +25,58 @@ class LoginView(APIView):
     throttle_classes = (AnonRateThrottle,)
 
     def post(self, request, format=None):
+        """
+        username -- A first parameter
+        password -- A second parameter
+        parameters:
+            - username: username
+              description: user's email address
+              required: true
+              type: string
+              paramType: form
+            - password: password
+              description: user's password
+              required: true
+              type: string
+        """
         if 'username' in request.POST and 'password' in request.POST:
             username = request.POST.get('username')
             password = request.POST.get('password')
 
             # We are using email as the username, authenticate with that instead of the username
-            user_account = self.authenticate_by_email(username, password)
-            if user_account is not None:
+            user = self.authenticate_by_email(username, password)
+            if user is not None:
                 # print 'is user active: ' + user.is_active
-                if user_account.user.is_active:
-
-                    # TODO: I'm going to split up the login into 2 steps, 1 for authentication to get the key
-                    # TODO: and 2 once I get the key I will get the user's basic details
-
+                if user.is_active:
                     # Get the user's existing token, if one doesn't exist generate a new one
-                    user_token, created = Token.objects.get_or_create(user=user_account.user)
+                    user_token, created = Token.objects.get_or_create(user=user)
+
+                    results = dict()
 
                     # Serialize the user and token so the data can be passed back to the caller
-                    serializer = TokenSerializer(user_token)
+                    token_serializer = TokenSerializer(user_token)
+                    results['token'] = token_serializer.data['key']
+                    results['user'] = token_serializer.data['user']
 
-                    return Response(serializer.data, status=status.HTTP_200_OK)
+                    # Determine if the user is part of a client cohort
+                    client_membership = self.get_client_membership(user)
+                    if client_membership is not None:
+                        client_membership_serializer = ClientMemberSerializer(client_membership)
+                        results['client'] = client_membership_serializer.data['client']
+
+                    # Get the user or system preferences
+                    user_prefs = self.get_user_preferences(user)
+                    if user_prefs is not None:
+                        results['preferences'] = user_prefs
+                    else:
+                        # Default System Preferences have not been setup
+                        message = {'message': 'The default System Preferences have not been setup.'}
+                        return Response(message, status=status.HTTP_412_PRECONDITION_FAILED)
+
+                    return Response(results, status=status.HTTP_200_OK)
                 else:
                     # This user is not active in the system
-                    message = {'message': 'Account has been disabled.'}
+                    message = {'message': 'This account is inactive.'}
                     return Response(message, status=status.HTTP_401_UNAUTHORIZED)
             else:
                 # Incorrect username/password combination
@@ -53,25 +86,49 @@ class LoginView(APIView):
         message = {'message': 'Missing username and/or password.'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
-    def authenticate_by_email(self, email, password):
+    @staticmethod
+    def authenticate_by_email(email, password):
         # We are using an email as the username, so we can't use django's built in authentication,
         # Let's authenticate by grabbing the user by email and checking the password
         try:
             # Grab the user by email, once we have the user we will pass in the username
             # to django's built in authentication system
-            user_account = UserAccountSetup.objects.get(user__email=email)
+            user_details = User.objects.get(email=email)
 
             # Use django's built in authentication
-            user = authenticate(username=user_account.user.username, password=password)
+            user = authenticate(username=user_details.username, password=password)
 
             if user is not None:
-                return user_account
+                return user
             else:
                 return None
-        except UserAccountSetup.DoesNotExist:
-            # TODO: Return JSON that front-end can read?
-            print 'User Account not setup for login'
+        except User.DoesNotExist:
             return None
+
+    @staticmethod
+    def get_client_membership(user):
+        try:
+            return ClientMembership.objects.get(user=user, active=True)
+        except ClientMembership.DoesNotExist:
+            return None
+
+    @staticmethod
+    def get_user_preferences(user):
+        try:
+            # Get the user's custom preferences
+            user_prefs = UserPreferences.objects.get(user=user)
+            user_prefs_serializer = UserPreferencesSerializer(user_prefs)
+            return user_prefs_serializer.data
+        except UserPreferences.DoesNotExist:
+            # The user has not setup custom preferences, get the default system preferences
+            try:
+                # System preferences are setup initially, it's a unique record so there should only be 1
+                # record at all times. Place in a try/catch for robustness
+                sys_prefs = SystemPreferences.objects.all()[0]
+                sys_prefs_serializer = SystemPreferencesSerializer(sys_prefs)
+                return sys_prefs_serializer.data
+            except SystemPreferences.DoesNotExist:
+                return None
 
 
 class LogoutView(APIView):
