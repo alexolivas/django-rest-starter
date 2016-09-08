2e267c953bd30f97c2671c7db0e736cd0ddd1687
Alex Olivas <alex.olivas11@gmail.com>
2016-09-08 16:44:34 -0400

    Temporary sign up functionality > to be used with Auth0 at one point

--- authentication/auth_urls.py
+++ authentication/auth_urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
+    # url(r'^signup/$', views.SignUp.as_view(), name="sign_up"),
     url(r'^login/', views.Login.as_view(), name='login'),
     url(r'^logout/', views.Logout.as_view(), name='logout'),
 ]
--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,6 +3,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        write_only_fields = ('password',)
+
+
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
