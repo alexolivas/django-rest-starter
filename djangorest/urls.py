a0c93766b1e9969003fe6ea959f951ce71b92430
Alex Olivas <alex.olivas11@gmail.com>
2016-10-25 15:19:35 -0500

    upgraded django rest swagger to version 2

--- djangorest/urls.py
+++ djangorest/urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 from authentication import auth_urls
 
-router = routers.DefaultRouter()
+schema_view = get_swagger_view(title='Django Rest Skeleton API')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^', schema_view),
     url(r'^v1/auth/', include(auth_urls)),
 
-    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
-admin.site.site_header = 'Django REST Skeleton Administration'
+admin.site.site_header = 'Django Rest Skeleton'
