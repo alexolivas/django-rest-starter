2a8eea3905f034321f2cf45c215ba3d17d1125ec
Alex Olivas <alex.olivas11@gmail.com>
2016-10-25 19:20:29 -0500

    Removed swagger and replaced it with drfdocs

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
+    'rest_framework_docs',
     'authentication',
     'profiles',
 ]
--- djangorest/urls.py
+++ djangorest/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework_swagger.views import get_swagger_view
+# from rest_framework import routers
 from authentication import auth_urls
+from profiles import profile_urls
 
-schema_view = get_swagger_view(title='Django Rest Skeleton API')
+# router = routers.DefaultRouter()
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    # url(r'^', include(router.urls)),
     url(r'^auth/', include(auth_urls)),
-    url(r'^docs/', schema_view),
+    url(r'^profile/', include(profile_urls)),
+
+    url(r'^docs/', include('rest_framework_docs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
-admin.site.site_header = 'Django Rest Skeleton'
+admin.site.site_header = 'Django Rest Starter'
