49285245120a21df4b2598fb0d8f65c16d452db1
Alex Olivas <alex.olivas11@gmail.com>
2016-10-26 11:14:24 -0500

    Adding token refresh and token verify to auth urls

--- authentication/auth_urls.py
+++ authentication/auth_urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
-    url(r'^login/', obtain_jwt_token),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
