7a8f1906c4625aa4125e88ce645cd9e13401778b
Alex Olivas <alex.olivas11@gmail.com>
2016-11-08 13:24:55 -0500

    ALLOWED_HOST environment variable

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
